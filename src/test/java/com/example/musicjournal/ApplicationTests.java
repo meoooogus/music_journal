7fe51d50d72ed06e999d97e8e90661ccc1c4956e
meoooogus <kdh031230@gmail.com>
2026-03-17 11:14:31 +0900

    Initial commit: Spring Boot project setup

---
+++ src/test/java/com/example/musicjournal/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musicjournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
