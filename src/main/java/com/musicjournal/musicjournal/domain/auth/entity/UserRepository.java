1dcc1fcb245e3414728754688d1ae184e066f9f4
meoooogus <kdh031230@gmail.com>
2026-03-19 18:47:30 +0900

    feat: UserRepository 인터페이스 생성

--- src/main/java/com/musicjournal/musicjournal/domain/auth/entity/UserRepository.java
+++ src/main/java/com/musicjournal/musicjournal/domain/auth/entity/UserRepository.java
@@ -1,4 +1,8 @@
 package com.musicjournal.musicjournal.domain.auth.entity;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Boolean existsByEmail(String email); // 존재 여부 확인
 }
