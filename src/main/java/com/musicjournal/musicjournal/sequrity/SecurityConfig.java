733f9a86dfba247d2a64545992e7d389a3f804a1
meoooogus <kdh031230@gmail.com>
2026-03-20 18:01:07 +0900

    chore: SecurityConfig 설정 수정 (테스트)

--- src/main/java/com/musicjournal/musicjournal/sequrity/SecurityConfig.java
+++ src/main/java/com/musicjournal/musicjournal/sequrity/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.musicjournal.musicjournal.sequrity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +13,16 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) {
+        http
+                .csrf(csrf -> csrf.disable()) // API 테스트로 CSRF 보호 비활성화
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                ); // 모든 요청 허용 (로그인/JWT 구현 X)
+
+        return http.build();
     }
 }
