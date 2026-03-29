6c57e9a07604acda1ebd89b969aa6a533e672529
meoooogus <kdh031230@gmail.com>
2026-03-29 15:22:17 +0900

    feat: dto 변환을 위한 from메서드 추가

--- src/main/java/com/musicjournal/musicjournal/domain/track/dto/TrackResDto.java
+++ src/main/java/com/musicjournal/musicjournal/domain/track/dto/TrackResDto.java
@@ -1,5 +1,6 @@
 package com.musicjournal.musicjournal.domain.track.dto;
 
+import com.musicjournal.musicjournal.domain.track.entity.Track;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -15,4 +16,18 @@ public class TrackResDto {
     private String albumId;
     private String artworkUrl;
     private Integer durationMs;
+
+    public static TrackResDto from(Track track) {
+
+        return TrackResDto.builder()
+                .spotifyId(track.getSpotifyId())
+                .title(track.getTitle())
+                .artistName(track.getArtistName())
+                .artistId(track.getArtistId())
+                .albumName(track.getAlbumName())
+                .albumId(track.getAlbumId())
+                .artworkUrl(track.getArtworkUrl())
+                .durationMs(track.getDurationMs())
+                .build();
+    }
 }
