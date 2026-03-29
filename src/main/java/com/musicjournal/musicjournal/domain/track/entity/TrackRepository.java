a7b19daf8b28cd8f9990ee4784b02cc4bedf0202
meoooogus <kdh031230@gmail.com>
2026-03-29 14:49:05 +0900

    feat: Track repo 추가

---
+++ src/main/java/com/musicjournal/musicjournal/domain/track/entity/TrackRepository.java
@@ -0,0 +1,11 @@
+package com.musicjournal.musicjournal.domain.track.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface TrackRepository extends JpaRepository<Track, Long> {
+
+    Optional<Track> findBySpotifyId(String spotifyId);
+}
