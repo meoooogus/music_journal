2bd1e3e47bf208907b2f0e3739aca967c7c5debd
meoooogus <kdh031230@gmail.com>
2026-05-04 11:49:07 +0900

    feat: 통계 조회용 query 추가 (RecordRepository)

--- src/main/java/com/musicjournal/musicjournal/domain/record/entity/RecordRepository.java
+++ src/main/java/com/musicjournal/musicjournal/domain/record/entity/RecordRepository.java
@@ -2,9 +2,12 @@ package com.musicjournal.musicjournal.domain.record.entity;
 
 import com.musicjournal.musicjournal.domain.auth.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 
 public interface RecordRepository extends JpaRepository<Record, Long> {
 
@@ -13,4 +16,21 @@ public interface RecordRepository extends JpaRepository<Record, Long> {
 
     // 전체 목록 조회
     List<Record> findByUser(User user);
+
+    // 이번 달 기록 수
+    @Query("SELECT COUNT(r) FROM Record r WHERE r.user = :user AND YEAR(r.recordedDate) = :year AND MONTH(r.recordedDate) = :month")
+    long countByUserAndYearMonth(@Param("user") User user, @Param("year") int year, @Param("month") int month);
+
+    // 아티스트별 기록 수
+    @Query("SELECT r.track.artistName, r.track.artistId, COUNT(r) FROM Record r WHERE r.user = :user GROUP BY r.track.artistName, r.track.artistId ORDER BY COUNT(r) DESC LIMIT :limit")
+    List<Object[]> findTopArtistsByUser(@Param("user") User user, @Param("limit") int limit);
+
+    // 트랙별 기록 수
+    @Query("SELECT r.track.title, r.track.spotifyId, r.track.artworkUrl, COUNT (r) FROM Record r WHERE r.user = :user GROUP BY r.track.spotifyId, r.track.title, r.track.artworkUrl ORDER BY COUNT(r) DESC LIMIT :limit")
+    List<Object[]> findTopTracksByUser(@Param("user") User user, @Param("limit") int limit);
+
+    // 앨범별 기록 수
+    @Query("SELECT r.track.albumId, r.track.albumName, r.track.artworkUrl, COUNT (r) FROM Record r WHERE r.user = :user GROUP BY r.track.albumId, r.track.albumName, r.track.artworkUrl ORDER BY COUNT(r) DESC LIMIT :limit")
+    List<Object[]> findTopAlbumByUser(@Param("user") User user, @Param("limit") int limit);
+
 }
