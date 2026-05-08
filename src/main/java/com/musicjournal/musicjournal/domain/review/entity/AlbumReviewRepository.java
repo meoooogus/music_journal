a98af3149ecc09f18dede19191b69a206f85a726
meoooogus <kdh031230@gmail.com>
2026-05-08 17:23:59 +0900

    feat: AlbumReview repo 추가

---
+++ src/main/java/com/musicjournal/musicjournal/domain/review/entity/AlbumReviewRepository.java
@@ -0,0 +1,18 @@
+package com.musicjournal.musicjournal.domain.review.entity;
+
+import com.musicjournal.musicjournal.domain.auth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AlbumReviewRepository extends JpaRepository<AlbumReview, Long> {
+
+    @Query("SELECT r FROM AlbumReview r JOIN FETCH r.recommendations WHERE r.album.spotifyAlbumId = :spotifyAlbumId")
+    List<AlbumReview> findReviewsBySpotifyAlbumId(@Param("spotifyAlbumId") String spotifyAlbumId);
+
+    @Query("SELECT r FROM AlbumReview r JOIN FETCH r.recommendations WHERE r.user = :user AND r.album.spotifyAlbumId = :spotifyAlbumId")
+    Optional<AlbumReview> findMyReview(@Param("user") User user, @Param("spotifyAlbumId") String spotifyAlbumId);
+}
