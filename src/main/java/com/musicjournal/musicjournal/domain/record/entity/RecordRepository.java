3bad528a96bd20f9acd6f50765949ff3f4c0657c
meoooogus <kdh031230@gmail.com>
2026-06-23 11:26:57 +0900

    chore: RecordRepository 미사용 import 제거

--- src/main/java/com/musicjournal/musicjournal/domain/record/entity/RecordRepository.java
+++ src/main/java/com/musicjournal/musicjournal/domain/record/entity/RecordRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Objects;
 
 public interface RecordRepository extends JpaRepository<Record, Long> {
 
